0bde58fad02dab96978b3d53b313d3064e26ed38
Caluff <dcaluff@gmail.com>
2023-04-03 22:06:56 -0400

    corrigiendo errores del material-ui osoleto, falta material-ui icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import {AppBar, Toolbar, Typography, Box, Autocomplete, InputBase} from "@mui/material";
-import SearchIcon from '@mui/icons-material'
+//import SearchIcon from '@mui/icons-material'
 import useStyles from './styles.js'
 const Header = () => {
     const classes = useStyles();
     return (
         <AppBar position={"static"}>
-            <Toolbar className={classes.toolbar}>
-                <Typography variant={"h5"} className={classes.title}>
+            <Toolbar /*className={classes.toolbar}*/>
+                <Typography variant={"h5"} /*className={classes.title}*/>
                     Travel Advisor
                 </Typography>
                 <Box display={"flex"}>
-                    <Typography variant={"h6"} className={classes.title}>
+                    <Typography variant={"h6"} /*className={classes.title}*/>
                         Explore new places
                     </Typography>
                     {/*<Autocomplete>*/}
